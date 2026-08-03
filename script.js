6fbe99c4daee68f28f7f120713c10de82c39d22d
snow <geoffreyw320@gmail.com>
2026-08-03 18:28:16 +0300

    feat:added event listener for the header when page is scrolled

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("#header");
+
+window.addEventListener("scroll", () =>{
+    if(window.scrollY > 80){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+});
