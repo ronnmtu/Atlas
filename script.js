d4f6ee657e418c0624df17c59829b06caae49bdd
snow <geoffreyw320@gmail.com>
2026-08-03 19:20:51 +0300

    feat:added slide change

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ window.addEventListener("scroll", () =>{
     }
 });
 
+// HERO SLIDES
+
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function changeSlide(){
+    slides[currentSlide].classList.remove("active");
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+}
+
+setInterval(changeSlide,7000);
